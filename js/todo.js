80827343b62191af6c70265a440280c3275f1461
jisun94 <jisun94lee@gmail.com>
2021-12-14 15:54:09 +0900

    ✨ Create delete button and make todo-list deleted

--- js/todo.js
+++ js/todo.js
@@ -4,11 +4,20 @@ const toDoForm = document.querySelector('#todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.querySelector('#todo-list');
 
+function deleteToDo(event) {
+  const clickedToDo = event.target.parentNode;
+  clickedToDo.remove();
+}
+
 function createToDo(newToDo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
+  const button = document.createElement('button');
   span.innerHTML = newToDo;
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
   li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
